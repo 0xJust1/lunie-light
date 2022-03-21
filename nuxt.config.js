edc56f9d4d57bbad7d50b044de54987911fef788
0xJust1 <82548334+0xJust1@users.noreply.github.com>
2022-03-21 16:24:07 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
         hid: 'description',
         name: 'description',
         content:
-          'Lunie 3 is a simple staking and governance interface for proof-of-stake blockchains',
+          'Dahsboard to stake, unstake, claim your DSM',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
