b814a66bf06154016a77f22e20313c9d6c6dd034
0xJust1 <82548334+0xJust1@users.noreply.github.com>
2022-03-21 16:20:54 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'Lunie 3',
+    title: 'Desmos - LetsRunSomeFuckingNode',
     htmlAttrs: {
       lang: 'en',
     },
